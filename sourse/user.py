1968de40d4eab2a29024feef5d85e14584fca4a3
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-06-10 19:14:32 -0300

    Created a User class to store its the lat and lon

---
+++ sourse/user.py
@@ -0,0 +1,18 @@
+
+# Classe responsável por armazenar a latitude
+# e longitude do usuário
+
+class User:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f'User({self.lat}, {self.lon})'
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def update_position(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
